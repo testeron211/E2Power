1cac85d52cd1a648dd039f84e62c40f632470338
Wesssed <116358813+Wesssed@users.noreply.github.com>
2023-04-05 20:03:47 +0300

    deleting errored nwbool

--- lua/entities/gmod_wire_expression2/core/custom/propCore.lua
+++ lua/entities/gmod_wire_expression2/core/custom/propCore.lua
@@ -259,12 +259,7 @@ e2function void entity:setPos(vector pos)
 	if !IsValid(this)  then return end
 	if blacklistedClasses[ this:GetClass() ] then error("[E2p]: недопустимый класс энтити entity:setPos()!") return end
 	if !isOwner(self, this) then return end
-	if this:IsPlayer() then
-		if !this:GetNWBool("_kyle_buildmode") and !self.player:IsAdmin() then 
-			error("[E2p]: нельзя использовать entity:setPos() в PVP!") 
-			return 
-		end
-	end
+
 	if !IsValidPos(pos) then return end
 	if validPhysics(this) then 
 		local phys = this:GetPhysicsObject()
